9f489398b8a217973cfa922cbd153dbfaf15cb36
Aarti Ahire <61686393+Aarti98765@users.noreply.github.com>
2020-03-20 10:11:01 +0530

    REFACTORED:Removed the spaces.

--- arithmeticComputation/arithmeticComputation.sh
+++ arithmeticComputation/arithmeticComputation.sh
@@ -1,43 +1,41 @@
 #!/bin/bash -x
 echo "Enter three numbers which required to perform arithmetic computation:" 
 read firstNumber secondNumber thirdNumber
-key=1
-computationAnsDictionary[key]=$(( $firstNumber+$secondNumber*$thirdNumber ))
- (( key ++ ))
-computationAnsDictionary[key]=$(( $firstNumber*$secondNumber+$thirdNumber ))
- (( key ++ ))
-computationAnsDictionary[key]=$(( $firstNumber+$secondNumber/$thirdNumber ))
- (( key ++ ))
-computationAnsDictionary[key]=$(( $firstNumber%$secondNumber+$thirdNumber ))
+
+key=1                                                                 #Performed arithmetic computation and stored the result
+computationAnsDictionary[key++]=$(( $firstNumber+$secondNumber*$thirdNumber ))
+computationAnsDictionary[key++]=$(( $firstNumber*$secondNumber+$thirdNumber ))
+computationAnsDictionary[key++]=$(( $firstNumber+$secondNumber/$thirdNumber ))
+computationAnsDictionary[key++]=$(( $firstNumber%$secondNumber+$thirdNumber ))
 echo "Values of dictionary:" ${computationAnsDictionary[@]}
 
 counter=0
-for value in ${computationAnsDictionary[@]}
+for value in ${computationAnsDictionary[@]}                          #Forloop is used to read dictionary values in array.
 do
-  computationAnsArray[$(( counter++ ))]=$value
+	computationAnsArray[$(( counter++ ))]=$value
 done
 echo "Values of array:" ${computationAnsArray[@]}
 
-for ((start=0; start<key; start++))
-do 
- for ((row=$(( $start+1 )); row<key; row++))
- do
-	if [[ computationAnsArray[start] -lt computationAnsArray[row] ]]
-	then
-	 tempVeriable=${computationAnsArray[start]}
-	 computationAnsArray[start]=${computationAnsArray[row]}
-	 computationAnsArray[row]=$tempVeriable
-	fi
- done
+for ((start=0; start<key; start++))                                  #Both forloop is used to perform sorting of an array
+do                                                                   #in a descending order.
+	for ((row=$(( $start+1 )); row<key; row++))
+	do
+		if [[ computationAnsArray[start] -lt computationAnsArray[row] ]]
+   		then
+    			tempVeriable=${computationAnsArray[start]}
+    			computationAnsArray[start]=${computationAnsArray[row]}
+    			computationAnsArray[row]=$tempVeriable
+   		fi
+ 	done
 done
 echo "Array element in descending order is:" ${computationAnsArray[@]}
 
 index=key 
 countOfArray=1
-while [[ $(( $index+1 )) -ne 0 ]]  
+while [[ $(( $index+1 )) -ne 0 ]]                                    #Whileloop is used to display element in ascending order. 
 do
-  sortedArrayDesending[countOfArray]=${computationAnsArray[index]}
-  (( countOfArray++ ))
-  (( index-- ))
+	sortedArrayDesending[countOfArray]=${computationAnsArray[index]}
+	(( countOfArray++ ))
+	(( index-- ))
 done
 echo "Array element in ascending order is:" ${sortedArrayDesending[@]}
